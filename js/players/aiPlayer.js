21fa6cf071d97051fd7cc2d3e6ffb771c085ff9a
zeevg <zeevg@wix.com>
2022-10-08 00:52:19 +0300

    ai player placeholder

--- js/players/aiPlayer.js
+++ js/players/aiPlayer.js
@@ -2,10 +2,13 @@ window["AiPlayer"] = (() => {
   class AiPlayer extends Player {
     constructor(name, color, startingPoint) {
       super(name, color, startingPoint);
+      this.state = AiStates.IDLE;
     }
 
     update(deltaTime, timestamp) {
       super.update(deltaTime, timestamp);
+      //search for enemy units
+      //if ()
     }
 
     draw(ctx) {
---
+++ js/players/aiState.js
@@ -0,0 +1,7 @@
+const AiStates = {
+    IDLE: 'IDLE',
+    SEARCHING: 'SEARCHING',
+    ATTACK: 'ATTACK',
+    MOVING: 'MOVING',
+    DEFEND: 'DEFEND',
+};
