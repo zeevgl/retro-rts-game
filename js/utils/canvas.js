1563c151da0bc8a953ed3d5659ef19a42f9a1e48
zeevg <zeevg@wix.com>
2022-12-12 18:11:33 +0200

    add missing text align

--- js/utils/canvas.js
+++ js/utils/canvas.js
@@ -1,5 +1,6 @@
 function drawText(ctx, text, x, y, color = "black", textAlign = "center", font = "12px Arial") {
     ctx.font = font;
     ctx.fillStyle = color;
+    ctx.textAlign = textAlign;
     ctx.fillText(text, x, y);
 }
